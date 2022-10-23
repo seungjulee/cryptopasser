266d09fa924a222a4fc1e52f2c48526411636cf2
SeungJu Lee <sjtogo@gmail.com>
2022-10-23 12:57:06 +0900

    Initial Commit

---
+++ cryptopass-signer/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button>Sign in</button>
+        <div>Create authentication token</div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
